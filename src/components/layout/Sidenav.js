4d0ad175aaddcfb6461815d97e0cf87f74b7388d
oyuerdenem <oyuerdenem@gmail.com>
2024-01-09 18:18:03 +0800

    initial commit

---
+++ src/components/layout/Sidenav.js
@@ -0,0 +1,91 @@
+/*!
+  =========================================================
+  * Muse Ant Design Dashboard - v1.0.0
+  =========================================================
+  * Product Page: https://www.creative-tim.com/product/muse-ant-design-dashboard
+  * Copyright 2021 Creative Tim (https://www.creative-tim.com)
+  * Licensed under MIT (https://github.com/creativetimofficial/muse-ant-design-dashboard/blob/main/LICENSE.md)
+  * Coded by Creative Tim
+  =========================================================
+  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// import { useState } from "react";
+import { Menu } from "antd";
+import { ShopOutlined, WalletOutlined, CarOutlined, SendOutlined, UsergroupAddOutlined, BarChartOutlined, ProfileOutlined, HddOutlined, FileTextOutlined} from '@ant-design/icons'
+import { NavLink } from "react-router-dom";
+import logo from "../../assets/images/logo.png";
+
+function Sidenav() {
+  return (
+    <>
+      <div className="brand">
+        <img src={logo} alt="" />
+        <span>Хайрцаг</span>
+      </div>
+      <hr />
+      <Menu theme="light" mode="inline">
+        <Menu.Item key="1">
+          <NavLink to="/dashboard">
+            <span className="icon"><ProfileOutlined /></span>
+            <span className="label">Нөөц</span>
+          </NavLink>
+        </Menu.Item>
+        <Menu.Item key="2">
+          <NavLink to="/tables">
+            <span className="icon"><HddOutlined /></span>
+            <span className="label">Агуулах</span>
+          </NavLink>
+        </Menu.Item>
+        <Menu.Item key="3">
+          <NavLink to="/billing">
+            <span className="icon"><UsergroupAddOutlined /></span>
+            <span className="label">Нийлүүлэгч</span>
+          </NavLink>
+        </Menu.Item>
+        <Menu.Item key="4">
+          <NavLink to="/rtl">
+            <span className="icon"><FileTextOutlined /></span>
+            <span className="label">Бараа</span>
+          </NavLink>
+        </Menu.Item>
+        <Menu.Item className="menu-item-header" key="5">
+          {/* Account Pages */}
+        </Menu.Item>
+        <Menu.Item key="6">
+          <NavLink to="/profile">
+            <span className="icon"><BarChartOutlined /></span>
+            <span className="label">Диаграмм</span>
+          </NavLink>
+        </Menu.Item>
+        <Menu.Item key="7">
+          {/* <NavLink to="/sign-in"> */}
+          <NavLink to="/store">
+            <span className="icon"><ShopOutlined /></span>
+            <span className="label">Дэлгүүр</span>
+          </NavLink>
+        </Menu.Item>
+        <Menu.Item key="8">
+          <NavLink to="/sign-up">
+            <span className="icon"><WalletOutlined /></span>
+            <span className="label">Борлуулалт</span>
+          </NavLink>
+        </Menu.Item>
+        <Menu.Item key="9">
+          <NavLink to="/sign-up">
+            <span className="icon"><CarOutlined /></span>
+            <span className="label">Хөдөлгөөн</span>
+          </NavLink>
+        </Menu.Item>
+        <Menu.Item key="10">
+          <NavLink to="/sign-up">
+            <span className="icon"><SendOutlined /></span>
+            <span className="label">Татан авалт</span>
+          </NavLink>
+        </Menu.Item>
+      </Menu>
+    </>
+  );
+}
+
+export default Sidenav;
---
+++ src/pages/Store.js
@@ -0,0 +1,166 @@
+import {
+  Row,
+  Col,
+  Card,
+  Radio,
+  Table,
+  Button,
+  Form,
+  Input,
+  Drawer,
+  Popconfirm,
+} from "antd";
+
+// Images
+import pencil from "../assets/images/pencil.svg";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const project = [
+  {
+    title: "НЭР",
+    dataIndex: "name",
+    width: "32%",
+  },
+  {
+    title: "БАЙРШИЛ",
+    dataIndex: "location",
+  },
+];
+
+function Store() {
+  const [store, setStore] = useState([]);
+  const [loadingStore, setLoadingStore] = useState(false)
+  const [isAddModal, setIsAddModal] = useState(false);
+  const [isUpdateModal, setIsUpdateModal] = useState(false)
+  const [row, setRow] = useState();
+
+  const getAll = () =>  {
+    setLoadingStore(true)
+    axios.get('http://localhost:3000/store').then(res => {
+      if (res.data.success) {
+        setStore(res.data.values);
+      }
+      setLoadingStore(false)
+    })
+  }
+
+  useEffect(() => {
+    getAll();
+  }, [])
+
+  const onChange = (e) => {
+  };
+
+  const handleAddStore = (values)=>{
+    axios.post('http://localhost:3000/store', values).then(res => {
+      console.log(res);
+      if(res.data.success){
+        getAll();
+        setIsAddModal(false)
+        // setStore(res.data.values);
+      }
+    })
+  }
+
+  const handleUpdateStore =(values) => {
+    axios.put('http://localhost:3000/store/' + row._id, values).then(res => {
+      if(res.data.success){
+        getAll();
+        setIsUpdateModal(false)
+      }
+    })
+  }
+
+  const handleClickUpdate = () => {
+    setIsUpdateModal(true);
+  }
+
+  const handleClickDelete = () => {
+    axios.delete('http://localhost:3000/store/' + row._id).then(res => {
+      if(res.data.success){
+        getAll();
+        setRow();
+      }
+    })
+  }
+
+  return (
+    <>
+      <div className="tabled">
+        <Row gutter={[24, 0]}>
+          <Col xs="24" xl={24}>
+            <Card
+              bordered={false}
+              className="criclebox tablespace mb-24"
+              title="Дэлгүүр"
+              extra={
+                <>
+                  <Radio.Group onChange={onChange} defaultValue="all">
+                    <Radio.Button value="all" onClick={getAll}>Бүгд</Radio.Button>
+                    <Radio.Button value="add" onClick={() => setIsAddModal(true)}>Нэмэх</Radio.Button>
+                    <Radio.Button disabled={!row} value="update" onClick={handleClickUpdate}>Засах</Radio.Button>
+                    <Popconfirm title="Are u sure!" onConfirm={handleClickDelete}>
+                    <Radio.Button disabled={!row} value="delete">Устгах</Radio.Button>
+                    </Popconfirm>
+                  </Radio.Group>
+                </>
+              }
+            >
+              <div className="table-responsive">
+                <Table
+                  columns={project}
+                  dataSource={store || []}
+                  pagination={false}
+                  rowKey={row => row._id}
+                  className="ant-border-space"
+                  loading={loadingStore || false}
+                  onRow={e => ({
+                    onClick: () => setRow(e)
+                  })}
+                  rowClassName={e => e._id === row?._id && 'active'}
+                />
+              </div>
+            </Card>
+          </Col>
+        </Row>
+
+
+        <Drawer title="Шинээр дэлгүүр бүртгэх" visible={isAddModal} onClose={() => setIsAddModal(false)} footer={false} destroyOnClose>
+          <Form layout="vertical" onFinish={handleAddStore}>
+            <Form.Item name="name" label="Нэр" rules={[{ required: true, message: 'Дэлгүүрийн нэрийг оруулна уу.' }]}>
+              <Input placeholder="Дэлгүүрийн нэр" autoFocus />
+            </Form.Item>
+
+            <Form.Item name="location" label="Байршил" rules={[{ required: true, message: 'Дэлгүүрийн байршлыг оруулна уу.' }]}>
+              <Input placeholder="Дэлгүүрийн байршил" />
+            </Form.Item>
+
+            <Form.Item>
+              <Button htmlType="submit" type="primary">Бүртгэх</Button>
+            </Form.Item>
+          </Form>
+        </Drawer> 
+        {/* drawer bish table gej c bolno */}
+
+        <Drawer title="Мэдээлэл засах" visible={isUpdateModal} onClose={() => setIsUpdateModal(false)} footer={false} destroyOnClose>
+          <Form layout="vertical" onFinish={handleUpdateStore} initialValues={row}>
+            <Form.Item name="name" label="Нэр" rules={[{ required: true, message: 'Дэлгүүрийн нэрийг оруулна уу.' }]}>
+              <Input placeholder="Дэлгүүрийн нэр" />
+            </Form.Item>
+
+            <Form.Item name="location" label="Байршил" rules={[{ required: true, message: 'Дэлгүүрийн байршлыг оруулна уу.' }]}>
+              <Input placeholder="Дэлгүүрийн байршил" />
+            </Form.Item>
+
+            <Form.Item>
+              <Button htmlType="submit" type="primary">Хадгалах</Button>
+            </Form.Item>
+          </Form>
+        </Drawer> 
+      </div>
+    </>
+  );
+}
+
+export default Store;
